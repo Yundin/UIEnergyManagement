880782eacb1ae9edae11d21052c7acc0afd63824
Vladislav Yundin <4yundin@gmail.com>
2020-04-18 11:00:38 +0300

    UIManager is now object

--- hierarchy-checker/src/main/java/com/yundin/hierarchy_checker/UIManager.kt
+++ hierarchy-checker/src/main/java/com/yundin/hierarchy_checker/UIManager.kt
@@ -11,33 +11,31 @@ import com.yundin.hierarchy_checker.analyzer.HierarchyAnalyzer
 import com.yundin.hierarchy_checker.outputter.LogOutputter
 import com.yundin.hierarchy_checker.outputter.RecommendationOutputter
 
-class UIManager {
+object UIManager {
 
-    companion object {
-        @JvmStatic
-        fun init(application: Application) {
+    @JvmStatic
+    fun init(application: Application) {
 
-            val outputter: RecommendationOutputter = LogOutputter()
-            val adviser: Adviser = RandomAdviser()
-            val analyzer: HierarchyAnalyzer = ConcreteHierarchyAnalyzer(adviser, outputter)
+        val adviser: Adviser = RandomAdviser()
+        val outputter: RecommendationOutputter = LogOutputter()
+        val analyzer: HierarchyAnalyzer = ConcreteHierarchyAnalyzer(adviser, outputter)
 
-            application.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-                override fun onActivityStarted(activity: Activity) {
-                    analyzer.analyzeDynamicHierarchy(getActivityRoot(activity))
-                }
+        application.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityStarted(activity: Activity) {
+                analyzer.analyzeDynamicHierarchy(getActivityRoot(activity))
+            }
 
-                override fun onActivityPaused(activity: Activity) {}
-                override fun onActivityDestroyed(activity: Activity) {}
-                override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
-                override fun onActivityStopped(activity: Activity) {}
-                override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
-                override fun onActivityResumed(activity: Activity) {}
-            })
-        }
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+            override fun onActivityResumed(activity: Activity) {}
+        })
+    }
 
 
-        private fun getActivityRoot(activity: Activity): View {
-            return activity.window.decorView.rootView
-        }
+    private fun getActivityRoot(activity: Activity): View {
+        return activity.window.decorView.rootView
     }
 }
