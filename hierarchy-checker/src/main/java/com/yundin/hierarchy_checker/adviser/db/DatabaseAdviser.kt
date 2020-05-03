a6aceb668fd1c9904ff9ad0e49ae1f694a6cef47
Vladislav Yundin <4yundin@gmail.com>
2020-05-03 15:58:08 +0300

    DatabaseAdviser's synchronization were removed It turned out that getReadableDatabase is synchronized on its own

--- hierarchy-checker/src/main/java/com/yundin/hierarchy_checker/adviser/db/DatabaseAdviser.kt
+++ hierarchy-checker/src/main/java/com/yundin/hierarchy_checker/adviser/db/DatabaseAdviser.kt
@@ -1,11 +1,9 @@
 package com.yundin.hierarchy_checker.adviser.db
 
 import android.content.Context
-import android.database.sqlite.SQLiteDatabase
 import android.os.Handler
 import android.os.Looper
 import com.yundin.hierarchy_checker.adviser.Adviser
-import java.lang.ref.WeakReference
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.SynchronousQueue
 import java.util.concurrent.ThreadPoolExecutor
@@ -14,7 +12,6 @@ import java.util.concurrent.TimeUnit
 class DatabaseAdviser(context: Context) : Adviser {
 
     private val helper: AdviceHelper = AdviceHelper(context)
-    private var database: WeakReference<SQLiteDatabase?> = WeakReference(null)
     private val executorService: ExecutorService = ThreadPoolExecutor(
         0,
         Integer.MAX_VALUE,
@@ -26,17 +23,8 @@ class DatabaseAdviser(context: Context) : Adviser {
 
     override fun findAlternativeAsync(viewName: String, callback: (String) -> Unit) {
         executorService.execute {
-            var databaseLocal = database.get()
-            if (databaseLocal == null) {
-                synchronized(this) {
-                    databaseLocal = database.get()
-                    if (databaseLocal == null) {
-                        databaseLocal = helper.readableDatabase
-                        database = WeakReference(databaseLocal)
-                    }
-                }
-            }
-            val advices = helper.getAdvices(databaseLocal!!, viewName)
+            val database = helper.readableDatabase
+            val advices = helper.getAdvices(database, viewName)
             mainHandler.post {
                 advices.forEach(callback)
             }
