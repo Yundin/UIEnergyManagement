53430fe2f1c33919476d27b79e6c580381f275b9
Vladislav Yundin <4yundin@gmail.com>
2020-05-18 16:08:06 +0300

    Writing time to cpu logs

--- estimation/estimation.sh
+++ estimation/estimation.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 TIME_MS=100
 VIEWS_COUNT=27
 
@@ -9,20 +11,22 @@ main() {
 
   for i in {0..$((VIEWS_COUNT - 1))} ; do
       echo "Testing view $i..."
-  #    adb shell dumpsys batterystats --reset || exit 2
+      adb shell dumpsys batterystats --reset || exit 2
       echo "Batterystats reset success"
 
+      NOW=$(date +'%Y-%m-%d %T')
       echo "Testing..."
       adb shell am instrument -w \
       -e class com.yundin.estimation.EstimationTest#testViewByIndex \
       -e index $i \
       -e time $TIME_MS \
-      com.yundin.estimation.test/com.yundin.estimation.MyTestRunner
+      com.yundin.estimation.test/com.yundin.estimation.MyTestRunner || exit 5
       echo "Testing completed"
 
       echo "Dumping..."
       adb shell dumpsys batterystats > "results/battery_$i" || exit 3
-      adb shell dumpsys cpuinfo > "results/cpu_$i" || exit 4
+      echo $NOW > "results/cpu_$i"
+      adb shell dumpsys cpuinfo >> "results/cpu_$i" || exit 4
       echo "Dumpsys complete successfully\n"
   done
 }
